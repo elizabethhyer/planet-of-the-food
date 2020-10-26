63f01f6cbb9e323c2c6354d17dd821d3dcf411d5
Elizabeth Hyer <hyerhandmade@gmail.com>
2020-10-26 17:33:59 -0400

    adds #create and #ood_params methods in oods_controller

--- planet-of-the-food-backend/app/controllers/oods_controller.rb
+++ planet-of-the-food-backend/app/controllers/oods_controller.rb
@@ -4,4 +4,15 @@ class OodsController < ApplicationController
         render json: OodSerializer.new(Ood.all)
     end 
 
+    def create 
+        ood = Ood.create(ood_params)
+        render json: OodSerializer.new(ood)
+    end 
+
+    private 
+
+    def ood_params
+        params.require(:ood).permit(:name)
+    end 
+
 end
