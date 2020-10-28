9692f91c3981b0a6b75ae3edae654d8e96c3c3d5
Elizabeth Hyer <hyerhandmade@gmail.com>
2020-10-27 23:29:01 -0400

    adds #create and #recipe_params method to recipes_controller

--- planet-of-the-food-backend/app/controllers/recipes_controller.rb
+++ planet-of-the-food-backend/app/controllers/recipes_controller.rb
@@ -1,2 +1,14 @@
 class RecipesController < ApplicationController
+
+    def create 
+        recipe = recipe.create(recipe_params)
+        render json: RecipeSerializer.new(recipe)
+    end 
+
+    private 
+
+    def recipe_params
+        params.require(:recipe).permit(:name, :content, :ood_id)
+    end 
+
 end
