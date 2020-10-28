1bb27cc95970f277d9a5aaf77a484973ca81feae
Elizabeth Hyer <hyerhandmade@gmail.com>
2020-10-28 01:07:13 -0400

    adds reset and addListeners functions to index.js

--- planet-of-the-food-frontend/src/index.js
+++ planet-of-the-food-frontend/src/index.js
@@ -1,5 +1,27 @@
+const header = document.getElementById('header')
 const form = document.getElementById('oodForm')
+const mainContainer = document.getElementById('container')
 
 form.addEventListener('submit', Ood.createOod)
+header.addEventListener('click', reset)
+
+function reset() {
+    mainContainer.innerHTML = ""
+    mainContainer.innerHTML += `<form id="oodForm">
+    <label for="">Name:</label>
+    <input type="text" id="oodName">
+    <input type="submit">
+    </form>`
+    addListeners()
+}
+
+function addListeners() {
+    const form = document.getElementById('oodForm')
+    const div = document.createElement('div')
+    div.id = "oodContainer"
+    mainContainer.append(div)
+    form.addEventListener('submit', Ood.createOod)
+    Ood.renderOods()
+}
 
 Ood.fetchOods()
