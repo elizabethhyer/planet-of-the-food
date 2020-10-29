7b67ec561c23776c30d31e012bd68034d82ebc6c
Elizabeth Hyer <hyerhandmade@gmail.com>
2020-10-29 14:17:17 -0400

    limits routes in routes.rb

--- planet-of-the-food-backend/config/routes.rb
+++ planet-of-the-food-backend/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :recipes
-  resources :oods
+  resources :recipes, only: [:create]
+  resources :oods, only: [:index, :create, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
